0a90fd879c9a57bf3aab7644d20b5936dc5d1175
Ad-Amrit <adhikariamrit98271@gmail.com>
2023-02-22 00:48:30 +0545

    small change

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     EditText ed1;
     Button b1;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,32 +29,32 @@ public class MainActivity extends AppCompatActivity {
         b1=findViewById(R.id.btn1);
 
         String[] from = {"USD"};
-        ArrayAdapter ad = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,from);
+        ArrayAdapter<String> ad = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,from);
         sp1.setAdapter(ad);
 
         String[] to = {"NPR","INR"};
-        ArrayAdapter ad1 = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,from);
+        ArrayAdapter<String> ad1 = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,from);
         sp2.setAdapter(ad1);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
 
-                Double tot;
-                Double amount = Double.parseDouble(ed1.getText().toString());
-                if(sp1.getSelectedItem().toString() == "USD"&& sp2.getSelectedItem().toString() =="NPR")
+                double tot;
+                double amount = Double.parseDouble(ed1.getText().toString());
+                if(sp1.getSelectedItem().toString().equals("USD") && sp2.getSelectedItem().toString().equals("NPR"))
                 {
                     tot = amount * 132.58 ;
-                    Toast.makeText(getApplicationContext(),toy.toString(),Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), Double.toString(tot),Toast.LENGTH_LONG).show();
                 }
-                else if(sp1.getSelectedItem().toString() == "USD"&& sp2.getSelectedItem().toString() =="INR")
+                else if(sp1.getSelectedItem().toString().equals("USD") && sp2.getSelectedItem().toString().equals("INR"))
                 {
                     tot = amount * 82.89 ;
-                    Toast.makeText(getApplicationContext(),toy.toString(),Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), Double.toString(tot),Toast.LENGTH_LONG).show();
                 }
 
             }
-        })
+        });
 
     }
 }
